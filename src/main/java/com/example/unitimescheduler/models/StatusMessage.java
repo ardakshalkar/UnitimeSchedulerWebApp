8ab22e4d85ada05102e6268fa987c39fe3e0dc02
bakiir <bkiranarbek@gmail.com>
2025-07-10 21:43:13 +0600

    Modified Status Object to Serializable Status Object

--- src/main/java/com/example/unitimescheduler/models/StatusMessage.java
+++ src/main/java/com/example/unitimescheduler/models/StatusMessage.java
@@ -1,14 +1,24 @@
 package com.example.unitimescheduler.models;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
-public class StatusMessage {
+public class StatusMessage{
+    @JsonProperty("jobId")
     private String jobId;
+
+    @JsonProperty("status")
     private String status;
-    private String resultFile;
+
+    @JsonProperty("message")
     private String message;
+
+    @JsonProperty("resultFile")
+    private String resultFile;
 } 
